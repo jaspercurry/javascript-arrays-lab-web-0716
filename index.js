cfd751d315752a2c09ba269feb4edc32649d4307
jaspercurry <jaspercurry@gmail.com>
2016-08-31 14:03:27 -0400

    made lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kit1 = [...kittens, name]
+  return kit1
+}
+
+function prependKitten(name) {
+  var kit1 = [name, ...kittens]
+  return kit1
+}
+
+function removeLastKitten() {
+  var kit1 = kittens.slice(0, -1)
+  return kit1
+}
+
+function removeFirstKitten() {
+  var kit1 = kittens.slice(1)
+  return kit1
+}
